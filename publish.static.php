4c40fedbdafba4662595e6472c8da684d004dea5
vinceMeens <v.w.a.meens@comcordis.com>
2013-09-11 01:28:59 +0200

    Static publishing per project

---
+++ publish.static.php
@@ -0,0 +1,13 @@
+<?php
+
+XXX_Static_Publisher::publishMergedFilesFromOtherProject($project, $deployIdentifier, array
+(
+	'XXX.js',
+	'XXX_JS.js',
+	'XXX_EventDispatcher.js',
+	'XXX_URI.js',
+	'XXX_Timer.js'
+),
+'initialize.js');
+
+?>
